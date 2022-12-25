6e28cda4d12b3f48df39f357049b10d9470962ea
Aki78 <kmorooka78@gmail.com>
2022-12-25 12:56:50 +0200

    added x image to delete mode

--- Delete.gd
+++ Delete.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
+func _ready():
+	$ColorRect.hide()
+	$ColorRect2.hide()
+	
 
 func _input(event):
 	
 	if event.is_action_pressed("delete") and Singleton.mode == "normal":
 		Singleton.mode = "delete"
+		$ColorRect.show()
+		$ColorRect2.show()
 	if event.is_action_pressed("grab") and Singleton.mode == "normal":
 		Singleton.mode = "grab"
+
 		for area in get_overlapping_areas():
 			area.set_grab()
 		
@@ -18,11 +25,13 @@ func _input(event):
 				area.queue_free()
 			if area.is_in_group("wires"):
 				get_tree().call_group(area.group_name, "queue_free")
-	
+	if event.is_action_pressed("escape"):
+		$ColorRect.hide()
+		$ColorRect2.hide()
 	
 func set_grab():
 	pass
 	
 
-func _process(delta):
+func _physics_process(delta):
 	position = get_global_mouse_position()
