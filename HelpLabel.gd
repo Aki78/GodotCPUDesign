b6165ec408b4054ddc626e6de267da4e542fe7e1
Aki78 <kmorooka78@gmail.com>
2022-12-25 15:17:09 +0200

    small visual changes

--- HelpLabel.gd
+++ HelpLabel.gd
@@ -2,4 +2,4 @@ extends Label
 
 
 func _ready():
-	text = "wire mode: w\nnormal mode: Esc, q, right_click\nswitch mode: s\ndelete mode: x, d\ngate mode: l (for logic)" 
+	text = "wire mode: w\nnormal mode: Esc, q, right_click\nswitch mode: s\ndelete mode: x, d\ngate mode: l (for logic)\ninput: i" 
