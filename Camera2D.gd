00d9822daa2dd01b9cf7f942be575a82117f358f
Aki78 <kmorooka78@gmail.com>
2022-12-25 13:10:14 +0200

    added help label

--- Camera2D.gd
+++ Camera2D.gd
@@ -3,23 +3,27 @@ extends Camera2D
 var mouse_down = false
 var mouse_position_before
 
-var label_pos = Vector2(-450,250)
+export var label_pos : Vector2 = Vector2(-450,250)
+export var help_pos : Vector2 = Vector2(400,-250)
 
 signal zoom_in
 signal zoom_out
 
 func ready():
 	$ModeLabel.rect_position = label_pos
+	$HelpLabel.rect_position = help_pos
 
 func _input(e):
 	if Input.is_action_pressed("zoomin") and zoom.x > 0.1:
 		zoom *= 0.9
 		$ModeLabel.rect_scale *= 0.9
+		$HelpLabel.rect_scale *= 0.9
 		emit_signal("zoom_in")
 	if Input.is_action_pressed("zoomout") and zoom.x < 10:
 		emit_signal("zoom_out")
 		zoom *= 1.1
 		$ModeLabel.rect_scale *= 1.1
+		$HelpLabel.rect_scale *= 1.1
 	if Input.is_action_pressed("middle_mouse"):
 		mouse_down = true
 	else:
@@ -32,3 +36,4 @@ func _process(delta):
 
 	mouse_position_before = mouse_position_now
 	$ModeLabel.rect_position = offset + zoom*label_pos
+	$HelpLabel.rect_position = offset + zoom*help_pos
--- Game.gd
+++ Game.gd
@@ -1,17 +1,13 @@
 extends Node2D
 
-#
-#onready var camera = $Camera2D
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-#	camera.connect("zoom_in",self, "on_zoom_in")
-#	camera.connect("zoom_out",self, "on_zoom_out")
-#
-#
-#
-#func on_zoom_in():
-#	$ModeLabel.rect_scale *= 0.9
-#
-#func on_zoom_out():
-#	$ModeLabel.rect_scale *= 1.1
+func _ready():
+	$Help.hide()
+
+
+func _input(event):
+	if event.is_action_pressed("help"):
+		$Main.hide()
+		$Help.show()
+	if event.is_action_pressed("escape"):
+		$Help.hide()
+		$Main.show()
